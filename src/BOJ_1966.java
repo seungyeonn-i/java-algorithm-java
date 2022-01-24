905bff1a0ce3494b5d18e9a4e5caceb04daaa363
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-01-24 23:47:03 +0900

    ☑️ January 5week

--- src/BOJ_1966.java
+++ src/BOJ_1966.java
@@ -1,19 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class BOJ_1966 {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
+        int total = scanner.nextInt();
+        int n,m,count;
 
-        int a = Integer.parseInt(br.readLine());
-        int m;
-        int [] arr;
+        for(int i=0;i<total;i++){
+            LinkedList<int[]> queue = new LinkedList<>();
+            count = 0;
+            n = scanner.nextInt();
+            m = scanner.nextInt();
 
-        for(int i=0;i<a;i++){
+            for(int j=0;j<n;j++)
+                queue.add(new int[] {j,scanner.nextInt()});
 
+            while(!queue.isEmpty()){    // 다 검사
+                int[] now = queue.poll();
+                boolean able = true;
+
+                for(int [] q : queue)
+                    if(q[1] > now[1])   //더 중요한게 있다면
+                        able = false;
+                    if(able){
+                         count ++;
+                         if(now[0] == m)    break;
+                    }
+                    else
+                        queue.add(now);
+            }
+            System.out.println(count);
 
         }
 
